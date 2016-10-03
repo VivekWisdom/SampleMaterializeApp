32aa3eb4ee1adb5683d0fb753b6bdadc1d8c6593
Vivek Wisdom <vivek.wisdom@accenture.com>
2016-10-03 17:34:59 +0530

    Added dashboard Page

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import {MaterializeDirective} from 'angular2-materialize'
 
 
 @Component({
   selector: 'dashboard',
-  styleUrls: [`dashboard.css`],
-  templateUrl:'dashboard.html'
+  styleUrls: ['./dashboard.css'],
+  templateUrl:'./dashboard.html',
+  providers:[MaterializeDirective]
 })
 export class Dashboard {
   constructor() {
